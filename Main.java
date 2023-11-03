c480f6757ec7735d603b7eb3517b211086d5ce97
Matti Räty <mattiraty@hotmail.com>
2023-11-03 14:07:16 +0200

    created printOptions method

--- Main.java
+++ Main.java
@@ -2,8 +2,24 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static void printOptions() {
+        System.out.println("Choose units to convert:");
+        System.out.println("Fahrenheit to Celcius");
+        System.out.println("Celcius to Fahrenheit");
+        System.out.println("Celcius to Kelvin");
+        System.out.println("Kelvin to Celcius");
+        System.out.println("Fahrenheit to Kelvin");
+        System.out.println("Kelvin to Fahrenheit");
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println(TemperatureConverter.convertTemperature("Celcius to Fahrenheit", 23));
+        Boolean running = true;
+
+        while (running) {
+            
+           
+        }
+
     }
 }
