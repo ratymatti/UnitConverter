c4472ff32a7ab939f6d404eae452b7a5b194d297
Matti Räty <mattiraty@hotmail.com>
2023-11-03 14:20:41 +0200

    created getUnits method

--- Main.java
+++ Main.java
@@ -4,12 +4,31 @@ public class Main {
 
     public static void printOptions() {
         System.out.println("Choose units to convert:");
-        System.out.println("Fahrenheit to Celcius");
-        System.out.println("Celcius to Fahrenheit");
-        System.out.println("Celcius to Kelvin");
-        System.out.println("Kelvin to Celcius");
-        System.out.println("Fahrenheit to Kelvin");
-        System.out.println("Kelvin to Fahrenheit");
+        System.out.println("1. Fahrenheit to Celcius");
+        System.out.println("2. Celcius to Fahrenheit");
+        System.out.println("3. Celcius to Kelvin");
+        System.out.println("4. Kelvin to Celcius");
+        System.out.println("5. Fahrenheit to Kelvin");
+        System.out.println("6. Kelvin to Fahrenheit");
+    }
+
+    public static String getUnits(int selection) {
+        switch(selection) {
+            case 1:
+                return "Fahrenheit to Celcius";
+            case 2:
+                return "Celcius to Fahrenheit";
+            case 3:
+                return "Celcius to Kelvin";
+            case 4:
+                return "Kelvin to Celcius";
+            case 5:
+                return "Fahrenheit to Kelvin";
+            case 6:
+                return "Kelvin to Fahrenheit";
+            default:
+                return "Error.";         
+        }
     }
 
     public static void main(String[] args) {
@@ -17,8 +36,11 @@ public class Main {
         Boolean running = true;
 
         while (running) {
-            
-           
+            printOptions();
+            System.out.print("Enter your choise: ");
+            int selectedUnits = scanner.nextInt();
+            System.out.print("Enter temperature you want to convert: ");
+            double temperature = scanner.nextDouble(); 
         }
 
     }
